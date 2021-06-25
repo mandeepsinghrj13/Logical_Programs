89d4154d445031df92e07616646392b662bebe2a
mandeep <singh.mr.1996@gmail.com>
2021-06-25 17:43:54 +0530

    Write a code for Fibonacci Series

--- src/com/bridgelabz/FibonacciSeries.java
+++ src/com/bridgelabz/FibonacciSeries.java
@@ -1,10 +1,24 @@
 package com.bridgelabz;
-
-public class FibonacciSeries {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+import java.util.Scanner;
+public class FibonacciSeries 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcomr to Bridgelabz Logical Programs");
+		Scanner Scanner = new Scanner(System.in);
+		System.out.println("Enter The Number Find Fibonacci Series");
+		int previousNumber = 0 ;
+		int currentNumber = 1 ;
+		int fibonacciNumber = Scanner.nextInt();
+		System.out.print(previousNumber+","+currentNumber);
+		int nextNumber;
+		for (int i =0; i < fibonacciNumber - 2; i++ ) 
+		{
+			nextNumber = previousNumber + currentNumber;
+			previousNumber = currentNumber;
+			currentNumber = nextNumber;
+			System.out.print(","+nextNumber);
+		}
 	}
 
 }
